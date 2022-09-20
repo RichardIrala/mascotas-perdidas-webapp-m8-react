8b18a7ba229c692bf1852d906cff9372ecd9f56f
RichardIrala <richardirala47@hotmail.com>
2022-09-20 15:03:09 -0300

    Se agrego la página 'componentes' al Router

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,14 +1,17 @@
 import { Layout } from "components/Layout";
+import { Componentes } from "pages/Componentes";
+import { Homepage } from "pages/Homepage";
 import React from "react";
-import { Routes, Route, Link, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Outlet />}>
         <Route path="hola" element={<h1>Soy el path hola</h1>} />
-        <Route index element={<h1>Soy el index</h1>} />
+        <Route index element={<Homepage />} />
       </Route>
+      <Route path="componentes" element={<Componentes />} />
       <Route path="*" element={<h1>No existe la page bro</h1>} />
     </Routes>
   );
