63120ba96ed253e3f12cd0b0aab56d6b8715714f
RichardIrala <richardirala47@hotmail.com>
2022-09-20 17:39:33 -0300

    Se agrego el Header al Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Header } from "./Header";
 export const Layout = () => {
-  return <Outlet />;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
