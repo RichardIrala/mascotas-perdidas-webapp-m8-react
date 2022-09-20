0565e9a16ab0bf1df3154b59ec20bc858dc0b549
RichardIrala <richardirala47@hotmail.com>
2022-09-20 15:02:41 -0300

    Página 'Componentes' con componentes de la app instanciada

---
+++ src/pages/Componentes.tsx
@@ -0,0 +1,24 @@
+import { Header } from "components/Header";
+import React from "react";
+import { ButtonRose } from "ui/ButtonRose";
+import { GeneralText } from "ui/GeneralText";
+import { Title } from "ui/Title";
+
+export const Componentes = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", margin: 20 }}>Componentes</h1>
+      <Header />
+      <br />
+      <ButtonRose>Boton Rosa</ButtonRose>
+      <br />
+      <Title>Soy un titulo</Title>
+      <br />
+      <Title centred>Soy un titulo centrado</Title>
+      <br />
+      <GeneralText>Soy el texto general</GeneralText>
+      <br />
+      <GeneralText centred>Soy el texto general Centrado</GeneralText>
+    </div>
+  );
+};
