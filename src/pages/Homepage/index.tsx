817c4d5c99ca514c0dd3e9a3afbf042cba3c845a
RichardIrala <richardirala47@hotmail.com>
2022-09-20 17:40:15 -0300

    Se creo la página de inicio de la WebApp
    El boton que solicita la ubicación esta funcional.

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,13 +1,34 @@
 import { Header } from "components/Header";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { ButtonRose } from "ui/ButtonRose";
 import { GeneralText } from "ui/GeneralText";
 import { Title } from "ui/Title";
 import styles from "./index.css";
+
 export const Homepage = () => {
+  const navigate = useNavigate();
+
+  function getGeolocation() {
+    const aceptoGeoLoc = (position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      // Agregar aca un redirector hacia la ruta del welcome
+      navigate("/welcome");
+    };
+    const noAceptoGeoLoc = () => {
+      console.error("Acceso a la ubicación denegado");
+    };
+
+    if (!!navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        aceptoGeoLoc,
+        noAceptoGeoLoc
+      );
+    }
+  }
+
   return (
     <div>
-      <Header />
       <div className={styles["principal-box"]}>
         <div className={styles["secundary-box"]}>
           <div className={styles["title-container"]}>
@@ -25,19 +46,3 @@ export const Homepage = () => {
     </div>
   );
 };
-
-function getGeolocation() {
-  const aceptoGeoLoc = (position) => {
-    console.log(position.coords.latitude, position.coords.longitude);
-  };
-  const noAceptoGeoLoc = () => {
-    console.error("Acceso a la ubicación denegado");
-  };
-
-  if (!!navigator.geolocation) {
-    window.navigator.geolocation.getCurrentPosition(
-      aceptoGeoLoc,
-      noAceptoGeoLoc
-    );
-  }
-}
