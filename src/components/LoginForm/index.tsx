5d9229790069c18d82c7bd0fcfe4b311d8b013f4
RichardIrala <richardirala47@hotmail.com>
2022-09-25 03:42:00 -0300

    Se finalizo la funcionalidad del LoginForm
    Podría tener cambios a futuro. Pero ya funciona y hace su trabajo

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { LabelWithInput } from "components/LabelWithInput";
 import { ButtonRose } from "ui/ButtonRose";
 import styles from "./index.css";
+import { useRecoilState } from "recoil";
+import { userState } from "atoms";
+import { useNavigate } from "react-router-dom";
+import { API } from "helpers/API";
 
 export const LoginForm = () => {
+  const [userData, setUserData] = useRecoilState(userState);
+  const navigate = useNavigate();
+  let { email } = userData;
+
+  async function login(event) {
+    event.preventDefault();
+    let $password = event.target["password"].value.trim();
+
+    if (!$password || !email) {
+      alert("Se requieren los campos completos");
+    } else {
+      let result = await API.authLogin(email, $password);
+
+      if (!result.resjson.token) {
+        console.error("contraseña incorrecta");
+      } else {
+        setUserData({ ...userData, token: result.resjson.token });
+        navigate("/welcome");
+      }
+    }
+  }
+
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
+
   return (
-    <div className={styles.principal_container}>
+    <form
+      className={styles.principal_container}
+      onSubmit={(e) => {
+        login(e);
+      }}
+    >
       <LabelWithInput
         inputName="password"
         autocomplete="off"
@@ -13,13 +48,7 @@ export const LoginForm = () => {
       >
         Contraseña
       </LabelWithInput>
-      <ButtonRose
-        action={() => {
-          alert("funcionando");
-        }}
-      >
-        Confirmar
-      </ButtonRose>
-    </div>
+      <ButtonRose type="submit">Confirmar</ButtonRose>
+    </form>
   );
 };
