4bf96db4144d03a0be2013c19bdfa5d2a208b104
RichardIrala <richardirala47@hotmail.com>
2022-09-26 15:09:45 -0300

    Se agregaron el título y más contenido en la page MyPetsReported
    Se estilizo todo y ya esta funcionando visualmente. Solo se requiere la conexión al backend

--- src/pages/MyPetsReported/index.tsx
+++ src/pages/MyPetsReported/index.tsx
@@ -1,23 +1,31 @@
 import { PetCard } from "components/PetCard";
 import React from "react";
+import { GeneralText } from "ui/GeneralText";
+import { Title } from "ui/Title";
+import styles from "./index.css";
 const ref = [1, 2, 3, 4, 5];
+
 export const MyPetsReported = () => {
   return (
-    <div>
-      {ref?.map((pet, index) => {
-        return (
-          <PetCard
-            description="sep"
-            founded="true"
-            idPet={1}
-            last_location="Argentina"
-            petName="Argentinito"
-            pictureURL="https://res.cloudinary.com/richardiral/image/upload/v1662134184/h1mnrupvfcqtq4dif1x2.jpg"
-            key={index}
-            remove={"true"}
-          />
-        );
-      })}
+    <div className={styles.principalContainer}>
+      <Title centred>Mascotas reportadas por ti</Title>
+      <div className={styles.contenedor}>
+        {ref?.map((pet, index) => {
+          return (
+            <PetCard
+              description="sep"
+              founded="true"
+              idPet={1}
+              last_location="Argentina"
+              petName="Argentinito"
+              pictureURL="https://res.cloudinary.com/richardiral/image/upload/v1662134184/h1mnrupvfcqtq4dif1x2.jpg"
+              key={index}
+              remove={"true"}
+            />
+          );
+        })}
+      </div>
+      <GeneralText centred>No reportaste más mascotas</GeneralText>
     </div>
   );
 };
