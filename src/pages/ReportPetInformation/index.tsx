d420176e78d34cc38abe098ba6ac0e0b34648150
RichardIrala <richardirala47@hotmail.com>
2022-10-01 04:51:31 -0300

    Se modifico la página/componente de reportPetInformation y ahora funciona correctamente

--- src/pages/ReportPetInformation/index.tsx
+++ src/pages/ReportPetInformation/index.tsx
@@ -1,26 +1,45 @@
+import { petReportInfoIdState, petReportInfoState } from "atoms";
 import { LabelWithInput } from "components/LabelWithInput";
-import React from "react";
+import { usePetInfoForEmail } from "hooks/usePetInfoForEmail";
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { ButtonRose } from "ui/ButtonRose";
 import { Title } from "ui/Title";
 import styles from "./index.css";
+
 export const ReportPetInformation = () => {
-  const titulo = "cuentanos más sobre " + "jorgito";
-  const direccionDeLaImagen =
-    "https://res.cloudinary.com/richardiral/image/upload/v1662134184/h1mnrupvfcqtq4dif1x2.jpg";
+  const search = useLocation().search;
+  const titulo = "cuentanos más sobre ";
+  const idParam = new URLSearchParams(search).get("id");
+  const id = parseInt(idParam);
+  // const { id } = useQuery();
+  const petInfoForEmail = usePetInfoForEmail(id);
+  // useEffect temporal
+  useEffect(() => {
+    console.log(petInfoForEmail, "SOY toda la info");
+  }, [petInfoForEmail]);
+  
+
+  if (!Boolean(petInfoForEmail)) {
+    return <h1>Hubo un error... Esta mascota posiblemente no existe</h1>;
+  }
+
+  if (!Boolean(petInfoForEmail?.id)) {
+    return <h1>cargando...</h1>;
+  }
 
   return (
     <div className={styles["container"]}>
       <div className={styles["little-card-container"]}>
-        <Title>{titulo}</Title>
-        <img className={styles["pet-pic"]} src={direccionDeLaImagen} />
+        <Title centred>{titulo + petInfoForEmail.name}</Title>
+        <img className={styles["pet-pic"]} src={petInfoForEmail.pictureURL} />
       </div>
       <form className={styles["form"]}>
-        <LabelWithInput inputName="description">
+        <LabelWithInput inputName="description" autocomplete={"off"}>
           Información que sepas
         </LabelWithInput>
-        <button className={styles["form__button"]} type="submit">
-          <ButtonRose type="submit">Enviar</ButtonRose>
-        </button>
+        <ButtonRose type="submit">Enviar</ButtonRose>
       </form>
     </div>
   );
