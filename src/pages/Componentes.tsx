af45f0ba0509a0d7e99f09011e98bcbfd63c9e1a
RichardIrala <richardirala47@hotmail.com>
2022-10-01 04:56:37 -0300

    Página Componentes actualizada

--- src/pages/Componentes.tsx
+++ src/pages/Componentes.tsx
@@ -5,6 +5,7 @@ import { LoginForm } from "components/LoginForm";
 import { PetCard } from "components/PetCard";
 import { SignupForm } from "components/SignupForm";
 import React from "react";
+import { ButtonLogout } from "ui/ButtonLogout";
 import { ButtonRose } from "ui/ButtonRose";
 import { GeneralText } from "ui/GeneralText";
 import { Title } from "ui/Title";
@@ -29,10 +30,18 @@ export const Componentes = () => {
         description="si"
         founded="true"
         idPet={"4"}
-        last_location="corrientes"
+        last_location="Mock1"
         petName="Alfredo"
         pictureURL="https://res.cloudinary.com/richardiral/image/upload/v1662134184/h1mnrupvfcqtq4dif1x2.jpg"
-        remove="true"
+        remove={true}
+      />
+      <PetCard
+        description="si"
+        founded="true"
+        idPet={"4"}
+        last_location="Mock2"
+        petName="Alfredo 2"
+        pictureURL="https://res.cloudinary.com/richardiral/image/upload/v1662134184/h1mnrupvfcqtq4dif1x2.jpg"
       />
       <br />
       <LabelWithInput inputName="email" autocomplete="off">
@@ -45,6 +54,7 @@ export const Componentes = () => {
       <br />
       <SignupForm />
       <br />
+      <ButtonLogout />
     </div>
   );
 };
