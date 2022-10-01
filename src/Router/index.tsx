64069afa97f813bac6f24c87df15cc7838e337ad
RichardIrala <richardirala47@hotmail.com>
2022-10-01 04:44:34 -0300

    Se crearon 6 rutas nuevas
    Rutas: '/login', '/mis-datos', '/mis-mascotas-reportadas', '/reportar-mascota', '/reportar-informacion-de-mascota', '/signup'

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,17 +1,40 @@
+import { userState } from "atoms";
 import { Layout } from "components/Layout";
 import { CheckEmail } from "pages/CheckEmail";
 import { Componentes } from "pages/Componentes";
 import { Homepage } from "pages/Homepage";
+import { Login } from "pages/Login";
+import { MyPetsReported } from "pages/MyPetsReported";
+import { MyProfile } from "pages/MyProfile";
+import { ReportNewPet } from "pages/ReportNewPet";
+import { ReportPetInformation } from "pages/ReportPetInformation";
+import { Signup } from "pages/Signup";
 import { Welcome } from "pages/Welcome";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
 export const AppRoutes = () => {
+  const { token } = useRecoilValue(userState);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="welcome" element={<Welcome />} />
         <Route path="checkEmail" element={<CheckEmail />} />
+        <Route path="login" element={<Login />} />
+        <Route path="mis-datos" element={ !!token 
+          ? <MyProfile /> 
+          : <Navigate to={"/checkEmail"} />} />
+        <Route path="mis-mascotas-reportadas" element={!!token 
+          ? <MyPetsReported/> 
+          : <Navigate to={"/checkEmail"} />}/>
+        <Route path="reportar-mascota" element={!!token 
+          ? <ReportNewPet /> 
+          : <Navigate to={"/checkEmail"} />}/>
+        <Route path="reportar-informacion-de-mascota" element={!!token 
+          ? <ReportPetInformation /> 
+          : <Navigate to={"/checkEmail"} />}/>
+        <Route path="signup" element={<Signup />} />
         <Route index element={<Homepage />} />
       </Route>
       <Route path="componentes" element={<Componentes />} />
