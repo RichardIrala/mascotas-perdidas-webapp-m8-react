8d834736e06e949c74c947f40904387ebb2e4bc9
RichardIrala <richardirala47@hotmail.com>
2022-10-02 21:40:27 -0300

    Se creo el Loader de la aplicación

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./index.css";
+
+export const Loader = () => {
+  return (
+    <div className={styles["principal-container"]}>
+      <div className={styles["secundary-container"]}>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
